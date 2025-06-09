2f778c5406fa283e4185c94e0d3332c70d10efa2
Geert Custers <geert.aj.custers@gmail.com>
2025-06-09 12:53:25 +0000

    src/input_reader: do not use intrinsics

--- src/input_reader.cpp
+++ src/input_reader.cpp
@@ -3,7 +3,6 @@
 #include <rte_cpuflags.h>
 #include <stdio.h>
 #include <string.h>
-#include <x86intrin.h>
 
 #include <chrono>
 #include <iostream>
@@ -33,22 +32,17 @@ InputReader::InputReader(FILE* read_file) : InputReader(read_file, 1048576) { }
 
 InputReader::InputReader(FILE* read_file, const size_t block_size)
     : block_size(block_size),
-      process_buf_curr_ptr(NULL),
-      process_buf_end_ptr(NULL),
       simd_buf_curr_ptr(NULL),
       simd_buf_end_ptr(NULL),
       read_file(read_file),
       read_file_fd(fileno(read_file)),
       offset_tracker(0),
-      process_buf((char*) aligned_alloc(16, block_size), &::free),
-      read_buf((char*) aligned_alloc(16, block_size), &::free),
-      domain_begin_ptr(NULL),
+      process_buf(block_size),
+      read_buf(block_size),
+      process_buf_curr_cursor(process_buf.end()),
+      domain_begin_cursor(process_buf.end()),
       current_domain_valid(true),
-      curr_domain_written(0) {
-	// Check if the SSE4.2 instruction set is present, it is necessary for string processing
-	// instructions
-	if (!rte_cpu_get_flag_enabled(rte_cpu_flag_t::RTE_CPUFLAG_SSE4_2))
-		throw std::runtime_error("SSE4.2 instructions not supported on this CPU");
+      curr_domain_cursor(curr_domain.begin()) {
 
 	// The block size should be a power of two to make alignment easier
 	if (!IsPowerOfTwo(block_size))
@@ -79,7 +73,7 @@ InputReader::InputReader(FILE* read_file, const size_t block_size)
 	if (sqe == NULL)
 		throw std::runtime_error("No space available in ring");
 
-	io_uring_prep_read(sqe, read_file_fd, read_buf.get(), block_size, 0);
+	io_uring_prep_read(sqe, read_file_fd, read_buf.data(), block_size, 0);
 	// sqe->flags |= IOSQE_FIXED_FILE;
 
 	// The submit function should return that one SQE submission has been made
@@ -105,8 +99,9 @@ GetBufferResult InputReader::RefreshBuffers() {
 	size_t num_chars_read_from_file = cqe->res;
 
 	// Update the current and end pointer of the process buffer
-	process_buf_curr_ptr = process_buf.get();
-	process_buf_end_ptr = process_buf_curr_ptr + num_chars_read_from_file;
+	assert(num_chars_read_from_file < process_buf.size());
+	process_buf.resize(num_chars_read_from_file);
+	process_buf_curr_cursor = process_buf.begin();
 
 	// Keep track of the offset in the file for the next read operation
 	offset_tracker += num_chars_read_from_file;
@@ -120,7 +115,8 @@ GetBufferResult InputReader::RefreshBuffers() {
 		throw std::runtime_error("No space available in ring");
 
 	// Prepare sqe for read operation
-	io_uring_prep_read(sqe, read_file_fd, read_buf.get(), block_size, 0);
+	read_buf.resize(block_size);
+	io_uring_prep_read(sqe, read_file_fd, read_buf.data(), read_buf.size(), 0);
 	// sqe->flags |= IOSQE_FIXED_FILE;
 	sqe->off = offset_tracker;
 
@@ -137,13 +133,10 @@ InputReader::~InputReader() {
 
 /// \cond DO_NOT_DOCUMENT
 union m128i_chararr {
-	char chararr[16];
-	__m128i m128i;
 };
 
 union m128i_unsignedll {
 	unsigned long long unsignedll;
-	__m128i m128i;
 };
 /// \endcond
 
@@ -151,145 +144,47 @@ ReadDomainResult InputReader::GetDomain(DomainInputInfo& domain_info) {
 	GetBufferResult res = GetBufferResult::Success;
 	while (1) {
 		// First check if the process buffer needs to be refreshed
-		if (process_buf_curr_ptr == process_buf_end_ptr) {
+		if (process_buf_curr_cursor == process_buf.end()) {
 			// First write the remainder of the previous process_buf into the current
-			// domain
-			int chars_to_write =
-			    std::min(std::max(process_buf_end_ptr - domain_begin_ptr, (long int) 0),
-				(long int) (DOMAIN_NAME_MAX_SIZE - curr_domain_written - 1));
-			memcpy(curr_domain.data() + curr_domain_written, domain_begin_ptr,
-			    chars_to_write);
-			curr_domain_written += chars_to_write;
+			curr_domain_cursor = std::copy(domain_begin_cursor, process_buf.end(), curr_domain_cursor);
 
 			// Get a new process buffer and return if no new data is available
 			res = RefreshBuffers();
 			if (res == GetBufferResult::NotAvailable)
 				return ReadDomainResult::NotAvailable;
 			// Zero bytes read, end of file
-			if (process_buf_curr_ptr == process_buf_end_ptr)
+			if (process_buf_curr_cursor == process_buf.end())
 				return ReadDomainResult::FileEnd;
 
 			// New domain begin pointer is at the start of the process buffer
-			domain_begin_ptr = process_buf_curr_ptr;
+			domain_begin_cursor = process_buf.begin();
+			process_buf_curr_cursor = process_buf.begin();
 		}
 
-		// Check if the SIMD buffer has to be refreshed
-		if (simd_buf_curr_ptr == simd_buf_end_ptr) {
-			// Define the constants for filtering invalid characters
-			const m128i_chararr valid_chars = {"azAZ09..--__"};
-			const int op_valid_chars = _SIDD_CMP_RANGES | _SIDD_NEGATIVE_POLARITY;
+		if (*process_buf_curr_cursor == '\n') {
+			// First copy the (possibly partial) domain into the domain buffer.
+			curr_domain_cursor = std::copy(domain_begin_cursor, process_buf_curr_cursor, curr_domain_cursor);
+			*curr_domain_cursor++ = '\0';
 
-			// Filtering for \n and \0 directly is not possible since implicit
-			// string length is used for performance reasons. All characters except
-			// \n and \0 are selected and the output is negated
-			const m128i_chararr end_of_line_chars = {
-			    {('\0' + 1), ('\n' - 1), ('\n' + 1), (char) 0xFF}};
-			const int op_end_of_line = _SIDD_CMP_RANGES | _SIDD_NEGATIVE_POLARITY;
+			// Then copy it into the user provided buffer.
+			auto end = std::copy(curr_domain.begin(), curr_domain_cursor, domain_info.buf);
+			domain_info.len = end - domain_info.buf - 1;
+			auto result = current_domain_valid ? ReadDomainResult::Success : ReadDomainResult::NotValid;
 
-			m128i_unsignedll invalid_char_res;
-			m128i_unsignedll newline_res;
-
-			// Process all 64 bytes of the simd buffer for newlines and invalid
-			// characterrs in batches of 16 bytes
-			__m128i simd_data =
-			    _mm_loadu_si128(reinterpret_cast<__m128i_u*>(process_buf_curr_ptr));
-			invalid_char_res.m128i =
-			    _mm_cmpistrm(valid_chars.m128i, simd_data, op_valid_chars);
-			newline_res.m128i =
-			    _mm_cmpistrm(end_of_line_chars.m128i, simd_data, op_end_of_line);
-
-			simd_data = _mm_loadu_si128(
-			    reinterpret_cast<__m128i_u*>(process_buf_curr_ptr + 16));
-			invalid_char_res.m128i |=
-			    _mm_cmpistrm(valid_chars.m128i, simd_data, op_valid_chars) << 16;
-			newline_res.m128i |=
-			    _mm_cmpistrm(end_of_line_chars.m128i, simd_data, op_end_of_line) << 16;
-
-			simd_data = _mm_loadu_si128(
-			    reinterpret_cast<__m128i_u*>(process_buf_curr_ptr + 32));
-			invalid_char_res.m128i |=
-			    _mm_cmpistrm(valid_chars.m128i, simd_data, op_valid_chars) << 32;
-			newline_res.m128i |=
-			    _mm_cmpistrm(end_of_line_chars.m128i, simd_data, op_end_of_line) << 32;
-
-			simd_data = _mm_loadu_si128(
-			    reinterpret_cast<__m128i_u*>(process_buf_curr_ptr + 48));
-			invalid_char_res.m128i |=
-			    _mm_cmpistrm(valid_chars.m128i, simd_data, op_valid_chars) << 48;
-			newline_res.m128i |=
-			    _mm_cmpistrm(end_of_line_chars.m128i, simd_data, op_end_of_line) << 48;
-
-			simd_invalid_chars = invalid_char_res.unsignedll;
-			simd_newlines = newline_res.unsignedll;
-
-			simd_buf_curr_ptr = process_buf_curr_ptr;
-			simd_buf_end_ptr = std::min(process_buf_curr_ptr + 64, process_buf_end_ptr);
+			// Reset cursors
+			curr_domain_cursor = curr_domain.begin();
+			process_buf_curr_cursor++;
+			domain_begin_cursor = process_buf_curr_cursor;
+			return result;
 		}
 
-		// Get the location of the next newline character and the next invalid character
-		int loc_false_char_ctz = __builtin_ctzll(simd_invalid_chars);
-		int loc_newline_ctz = __builtin_ctzll(simd_newlines);
+		current_domain_valid &= (*process_buf_curr_cursor >= 'A' && *process_buf_curr_cursor <= 'Z') ||
+			(*process_buf_curr_cursor >= 'a' && *process_buf_curr_cursor <= 'z') ||
+			(*process_buf_curr_cursor >= '0' && *process_buf_curr_cursor <= '9') ||
+			*process_buf_curr_cursor == '-' || *process_buf_curr_cursor == '.' || *process_buf_curr_cursor == '_';
 
-		int loc_false_char = simd_invalid_chars ? loc_false_char_ctz : 64;
-		int loc_newline = simd_newlines ? loc_newline_ctz : 64;
-
-		// The location of the next valid character shouldn't be smaller than
-		// the location of the next newline, otherwise the domain is
-		// not valid
-		current_domain_valid &= loc_false_char >= loc_newline;
-
-		// Check if the newline location exceeds the end of the SIMD buffer
-		// otherwise indicate buffer refresh by updating pointers
-		if (simd_buf_curr_ptr + loc_newline < simd_buf_end_ptr) {
-			// First copy the first part of the domain to the buffer
-			memcpy(domain_info.buf, curr_domain.data(), curr_domain_written);
-
-			// Write the domain to the domain buffer, take any previously written
-			// characters into account and also an optional trailing dot.
-			const int max_size_domain_with_null =
-			    DOMAIN_NAME_MAX_SIZE - curr_domain_written - 2;
-			const int chars_to_write_unbounded =
-			    loc_newline + (simd_buf_curr_ptr - domain_begin_ptr);
-			const int& chars_to_write =
-			    std::min(chars_to_write_unbounded, max_size_domain_with_null);
-
-			// Check if the number of chars to write doesn't point to the maximum number
-			// of chars
-			current_domain_valid &= &chars_to_write_unbounded == &chars_to_write;
-
-			const int total_length = chars_to_write + curr_domain_written;
-
-			memcpy(domain_info.buf + curr_domain_written, domain_begin_ptr,
-			    chars_to_write);
-			domain_info.buf[total_length] = '\0';
-
-			domain_info.len = total_length;
-
-			// Reset character count for new domain
-			curr_domain_written = 0;
-
-			// Filter out the current newline and invalic character bits to be able to
-			// detect the next domain
-			simd_invalid_chars &= ((unsigned long long) 0xFFFFFFFFFFFFFFFE)
-			                      << loc_newline;
-			simd_newlines &= ((unsigned long long) 0xFFFFFFFFFFFFFFFE) << loc_newline;
-
-			// Update the begin pointer to the next domain
-			domain_begin_ptr = simd_buf_curr_ptr + loc_newline + 1;
-
-			ReadDomainResult to_return = current_domain_valid
-			                                 ? ReadDomainResult::Success
-			                                 : ReadDomainResult::NotValid;
-
-			// Reset the domain valid flag
-			current_domain_valid = true;
-
-			return to_return;
-		} else {
-			simd_buf_curr_ptr = simd_buf_end_ptr;
-			process_buf_curr_ptr = simd_buf_end_ptr;
-		}
+		process_buf_curr_cursor++;
 	}
 
-	return ReadDomainResult::Success;
+	return ReadDomainResult::NotAvailable;
 }
